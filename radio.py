3d090a7d5d83497d8c11d32d867f93ac42752984
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-04 14:52:22 -0700

    backlight play

--- radio.py
+++ radio.py
@@ -966,7 +966,7 @@ try:
             if screen_on and stream:
                 display_everything(stream)
 
-        time.sleep(10)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     if mpv_process:
