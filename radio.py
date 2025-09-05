55a970d1742b015b501cae3840c48f357d6f695a
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-04 21:47:31 -0700

    spacing for 3 line titles

--- radio.py
+++ radio.py
@@ -697,7 +697,7 @@ def display_one(name):
     elif len(info_lines) == 2 and len(title_lines) == 2 and name not in reruns:
         y_offset += 1
     elif len(info_lines) == 2 and len(title_lines) == 1 and name not in reruns:
-        y_offset -= 16
+        y_offset += 0
     elif len(info_lines) == 1 and len(title_lines) == 1 and name not in reruns:
         y_offset += 15
 
