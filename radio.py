298a60db623301f67da8165f31e0a2775261e59c
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-04 21:15:40 -0700

    spacing for 3 line titles

--- radio.py
+++ radio.py
@@ -677,8 +677,8 @@ def display_one(name):
     elif len(info) == 1 and len(title_lines) == 1: # if one title line and no other info
         y_offset = 55
     
-    if name not in reruns and len(title_lines) < 3: # adjust for live banner
-        y_offset = y_offset - 5
+    #if name not in reruns and len(title_lines) < 3: # adjust for live banner
+    #    y_offset = y_offset - 5
 
     for i in title_lines:
         draw.text((SHOW_INFO_X, SHOW_ROW_1_Y + y_offset), i, font=BIGGEST_FONT, fill=TEXT_COLOR)
