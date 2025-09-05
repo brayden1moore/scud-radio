e6120cbedcf67d12ec284d754c8dbe93acec7eda
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-04 21:50:27 -0700

    spacing for 3 line titles

--- radio.py
+++ radio.py
@@ -700,6 +700,9 @@ def display_one(name):
         y_offset += 14
     elif len(info_lines) == 1 and len(title_lines) == 1 and name not in reruns:
         y_offset += 15
+    elif len(info_lines) == 2 and len(title_lines) == 2: 
+        y_offset += 10
+
 
     for i in title_lines:
         draw.text((SHOW_INFO_X, SHOW_ROW_1_Y + y_offset), i, font=BIGGEST_FONT, fill=TEXT_COLOR)
