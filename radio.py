c1ccf0086c22002b3dc1d58661153cfa4ec36fe7
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-04 19:43:35 -0700

    volume

--- radio.py
+++ radio.py
@@ -787,7 +787,7 @@ def show_volume_overlay(volume):
         img = current_image.copy()
         draw = ImageDraw.Draw(img)
         
-        volume_bar_end = SCREEN_HEIGHT - int((volume / 150) * SCREEN_HEIGHT)
+        volume_bar_end = TOP_DIVIDER_Y - int((volume / 150) * TOP_DIVIDER_Y)
         volume_bar_end = max(volume_bar_end, TOP_DIVIDER_Y+2)
 
         draw.rectangle([
