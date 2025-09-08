2783a486f55bd34510ed7387ceb3aae83b988f31
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-07 20:39:55 -0700

    new splash

--- radio.py
+++ radio.py
@@ -800,7 +800,7 @@ def display_one(name):
     if len(info_lines) == 1 and len(title_lines) == 2 and name not in reruns:
         y_offset += 10
     elif len(info_lines) == 2 and len(title_lines) == 2 and name not in reruns:
-        y_offset -= 5
+        y_offset -= 10
     elif len(info_lines) == 2 and len(title_lines) == 1 and name not in reruns:
         y_offset += 14
     elif len(info_lines) == 1 and len(title_lines) == 1 and name not in reruns:
