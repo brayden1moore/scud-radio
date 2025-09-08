1093e4ad947920e609f6cab891aee0a192f9dbac
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-07 20:45:30 -0700

    new splash

--- radio.py
+++ radio.py
@@ -834,7 +834,7 @@ def display_one(name):
 
     if info_lines:
         for i in info_lines:
-            draw.text((SHOW_INFO_X, SHOW_ROW_1_Y + y_offset + 28), i, font=MEDIUM_FONT, fill=TEXT_COLOR_2)
+            draw.text((SHOW_INFO_X, SHOW_ROW_1_Y + y_offset + 20), i, font=MEDIUM_FONT, fill=TEXT_COLOR_2)
             y_offset += 20
 
     # battery
