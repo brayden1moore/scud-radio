ff79f20e3b903f402e524d59bcbc86caaa2a484b
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-13 18:05:48 -0700

    add shutdown

---
+++ shutdown.py
@@ -0,0 +1,35 @@
+import driver as LCD_2inch
+from PIL import Image, ImageDraw, ImageFont, ImageSequence
+import subprocess
+import spidev as SPI
+
+# 2 inch
+RST = 27
+DC = 25
+BL = 23
+bus = 0 
+device = 0 
+MAX_BL = 100
+SCREEN_WIDTH = 320
+SCREEN_HEIGHT = 240
+
+disp = LCD_2inch.LCD_2inch()
+disp.Init()
+disp.clear()
+disp.bl_DutyCycle(MAX_BL)
+
+def display_goodbye():
+    image = Image.new('RGBA', (SCREEN_WIDTH, SCREEN_HEIGHT))
+    for i in 'goodbye':
+            
+        bg = Image.open(f'assets/scud_{i}.png') 
+        image.paste(bg, (0, 0))
+        disp.ShowImage(image)
+
+    bg = Image.open(f'assets/scud_splash_2.png') 
+    image.paste(bg, (0, 0))
+    disp.ShowImage(image)
+
+display_goodbye()
+
+subprocess.run(['sudo','shutdown','-h','now'])
