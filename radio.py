0e07c0f0300bccf7ea2a4b8ee89924682e6918e4
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-20 21:17:54 -0700

    redesign

--- radio.py
+++ radio.py
@@ -579,14 +579,16 @@ def display_everything(name, update=False, readied=False):
         # prev and next
         prev = streams[prev_stream]['logo_60'].rotate(50, expand=True)
         next = streams[next_stream]['logo_60'].rotate(-50, expand=True)
-        if prev.mode != 'RGBA':
-            prev = prev.convert('RGBA')
-        if next.mode != 'RGBA':
-            next = next.convert('RGBA')
-        prev_rotated = prev.rotate(50, expand=True)
-        next_rotated = next.rotate(-50, expand=True)
-        image.paste(prev_rotated, (26, 140), prev_rotated)
-        image.paste(next_rotated, (210, 140), next_rotated)
+        prev_mask = Image.new('L', prev.size, 255).rotate(50, expand=True)
+        next_mask = Image.new('L', next.size, 255).rotate(-50, expand=True)
+
+        image.paste(prev, (26,140), prev_mask)
+        image.paste(next, (210, 140), next_mask)
+        if prev_stream in favorites:
+            image.paste(star_60.rotate(50, expand=True), (26, 140), prev_mask)
+        if next_stream in favorites:
+            image.paste(star_60.rotate(-50, expand=True), (210, 140), next_mask)
+
 
         # double prev and next
         double_prev = streams[double_prev_stream]['logo_60'].rotate(90, expand=True)
