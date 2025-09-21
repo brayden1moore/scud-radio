c4862dd92a1cd04c43aa3a53486a7da66f16ff18
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-20 21:05:58 -0700

    redesign

--- radio.py
+++ radio.py
@@ -580,7 +580,7 @@ def display_everything(name, update=False, readied=False):
         prev = streams[prev_stream]['logo_60'].rotate(50, expand=True)
         next = streams[next_stream]['logo_60'].rotate(-50, expand=True)
 
-        mask = Image.new('L', 60, 60)
+        mask = Image.new('L', (60, 60))
         image.paste(prev, (26,140), mask.rotate(50))
         image.paste(next, (210, 140), mask.rotate(-100))
         if prev_stream in favorites:
