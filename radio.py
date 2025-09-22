3443cc933653f75b89280505af1bebcc38fcc928
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-21 20:21:51 -0700

    redesign

--- radio.py
+++ radio.py
@@ -624,10 +624,10 @@ def display_everything(direction, name, update=False, readied=False):
             image.paste(double_next_star_40, (279, 194), double_next_star_40)
         if double_prev_stream not in reruns:
             double_prev_live_40 = live_40.copy().rotate(30, expand=True)
-            image.paste(double_prev_live_40, (51, 167), double_prev_live_40)
+            image.paste(double_prev_live_40, (-13, 194), double_prev_live_40)
         if double_next_stream not in reruns:
             double_next_live_40 = live_40.copy().rotate(-30, expand=True)
-            image.paste(double_next_live_40, (218, 167), double_next_live_40)
+            image.paste(double_next_live_40, (279, 194), double_next_live_40)
 
         safe_display(image) # display 
     
