39d2af9f55f2d336819c88841c6857abc82c7416
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-22 19:14:30 -0700

    redesign

--- radio.py
+++ radio.py
@@ -683,9 +683,9 @@ def display_everything(direction, name, update=False, readied=False):
         mark_start = stream_list.index(name) * mark_width
         mark_end = mark_start + mark_width
         #draw.rectangle([0, 233, 320, 240], fill=YELLOW)
-        draw.rectangle([mark_start, 234, mark_start + 6, 240], fill=BLACK)
-        draw.rectangle([mark_start, 217, mark_start + 6, 233], fill=YELLOW)
-        draw.rectangle([mark_start + 12, 217, round(mark_start + width(name, MEDIUM_FONT)) + 2, 233], fill=YELLOW)
+        draw.rectangle([mark_start, 234, mark_start + 3, 240], fill=BLACK)
+        draw.rectangle([mark_start, 217, mark_start + 3, 233], fill=YELLOW)
+        draw.rectangle([mark_start + 6, 217, round(mark_start + 6 + width(name, MEDIUM_FONT)) + 2, 230], fill=YELLOW)
         draw.text((mark_start + 13, 218), name, font=MEDIUM_FONT, fill=BLACK)
         safe_display(image) # display 
     
