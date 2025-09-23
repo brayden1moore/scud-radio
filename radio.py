10e43d5c93742a442e719fea62c8045535037504
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-23 10:16:45 -0700

    redesign

--- radio.py
+++ radio.py
@@ -898,7 +898,7 @@ def show_volume_overlay(volume):
         # border
         draw.rectangle([SCREEN_WIDTH-9, 0, SCREEN_WIDTH, SCREEN_HEIGHT], fill=YELLOW)
         # volume fill
-        draw.rectangle([SCREEN_WIDTH-7, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=YELLOW)
+        draw.rectangle([SCREEN_WIDTH-7, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=BLACK)
 
         #draw.rectangle([SCREEN_WIDTH-9, 215, SCREEN_WIDTH, SCREEN_HEIGHT], fill=YELLOW)
 
