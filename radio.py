4353390013aacd18054e0c7456578d804b1628d0
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-24 15:24:22 -0700

    favorite indicator

--- radio.py
+++ radio.py
@@ -636,7 +636,7 @@ def display_everything(direction, name, update=False, readied=False, pushed=Fals
 
         if name in favorites:
             image.paste(this_star, logo_position, this_star)
-            draw.rectangle([107, 120, 213, 222], outline=YELLOW, width=1)
+            draw.rectangle([107, 120, 213, 221], outline=YELLOW, width=3)
         if name not in reruns:
             draw.ellipse([15, 15, 28, 28], fill=RED)
             #image.paste(this_live, logo_position, this_live)
