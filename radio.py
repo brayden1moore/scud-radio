b4bae32434265a688908c7f42160a60238dcdd37
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-23 17:29:01 -0700

    redesign

--- radio.py
+++ radio.py
@@ -738,6 +738,9 @@ def display_one(name):
     else:
         trim_color = BLACK
 
+    first_pixel_color = (166, 166, 166)
+    trim_color = WHITE
+
     image = Image.new('RGBA',(320, 240), color=first_pixel_color)
     draw = ImageDraw.Draw(image)  
 
