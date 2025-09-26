077322338ab1f7a66804cab9ce7439cd77902a60
Brayden Moore <braydenmoore2001@gmail.com>
2025-09-25 21:40:51 -0700

    contrast

--- radio.py
+++ radio.py
@@ -435,7 +435,7 @@ def get_stream_list(streams):
         fav_start_idx = round(len(stream_list) / 2) - round(len(favorites) / 2)
         front_half = [i for i in stream_list if i not in favorites][:fav_start_idx]
         back_half = [i for i in stream_list if i not in favorites and i not in front_half]
-        stream_list =  back_half + sorted(favorites) + front_half
+        stream_list =  back_half + sorted(favorites, key=str.casefold) + front_half
     
     return stream_list
 
