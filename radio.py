f3530c5075e6611bc008f81ff42837a7f446e818
Brayden Moore <braydenmoore2001@gmail.com>
2025-11-21 23:45:17 -0800

    messing with layout

--- radio.py
+++ radio.py
@@ -729,11 +729,11 @@ def display_everything(direction, name, update=False, readied=False, pushed=Fals
         first_tick_start = padding
         bar_width = 2
         mark_start = tick_locations[stream]
-        fill = WHITE if name not in favorites else BLACK
-        draw.rectangle([mark_start, tick_bar_start + 2, mark_start + bar_width, tick_bar_start + 2 + tick_bar_height - 4], fill=WHITE)
+        current_fill = WHITE if name not in favorites else BLACK
+        draw.rectangle([mark_start, tick_bar_start + 2, mark_start + bar_width, tick_bar_start + 2 + tick_bar_height - 4], fill=current_fill)
         if readied:
             mark_start = tick_locations[name]
-            draw.rectangle([mark_start, tick_bar_start + 2, mark_start + bar_width, tick_bar_start + 2 + tick_bar_height - 4], fill=(150,150,150))
+            draw.rectangle([mark_start, tick_bar_start + 2, mark_start + bar_width, tick_bar_start + 2 + tick_bar_height - 4], fill=(175,175,175))
             
         safe_display(image)
     
