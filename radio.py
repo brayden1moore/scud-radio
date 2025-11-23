5f95398ac791af778b7a2b16a20fd48a6c5705c8
Brayden Moore <braydenmoore2001@gmail.com>
2025-11-22 22:11:12 -0800

    messing with volume on/off

--- radio.py
+++ radio.py
@@ -976,7 +976,7 @@ def show_volume_overlay(volume):
         # volume fill
         draw.rectangle([SCREEN_WIDTH-10, last_volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=BLACK)
         draw.rectangle([SCREEN_WIDTH-10, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=trim_color)
-        #draw.rectangle([SCREEN_WIDTH-10, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], width=1, outline=BLACK)
+        draw.rectangle([SCREEN_WIDTH-10, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], width=1, outline=BLACK)
 
         safe_display(img)
         time.sleep(0.005)
