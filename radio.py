de6f6aea46035dc6d68a128b4aa56b439541b1ae
Brayden Moore <braydenmoore2001@gmail.com>
2025-11-24 22:15:22 -0800

    styling

--- radio.py
+++ radio.py
@@ -975,7 +975,7 @@ def show_volume_overlay(volume):
             tick_start += tick_gap
 
         # volume fill
-        draw.rectangle([SCREEN_WIDTH-10, last_volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=BLACK)
+        draw.rectangle([SCREEN_WIDTH-10, last_volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=img_background)
         draw.rectangle([SCREEN_WIDTH-10, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], fill=trim_color)
         draw.rectangle([SCREEN_WIDTH-10, volume_bar_end, SCREEN_WIDTH, SCREEN_HEIGHT], width=1, outline=BLACK)
 
