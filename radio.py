ef6a35b9229f4805bf2b62e957bd9dd316f3c617
Brayden Moore <braydenmoore2001@gmail.com>
2025-12-04 15:20:43 -0800

    styling

--- radio.py
+++ radio.py
@@ -749,7 +749,7 @@ def display_everything(direction, name, update=False, readied=False, pushed=Fals
             first_tick_start = padding
             bar_width = 2
             mark_start = tick_locations[stream]
-            current_fill = BLUE if stream not in favorites else BLACK
+            current_fill = BLUE if stream not in favorites else BLUE
             draw.rectangle([mark_start, tick_bar_start + 2, mark_start + bar_width, tick_bar_start + 2 + tick_bar_height - 4], fill=current_fill)
             if readied:
                 mark_start = tick_locations[name]
