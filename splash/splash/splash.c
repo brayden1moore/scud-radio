eb9bc87ae3f9d6ed5b849eaa696e88c65244e7d6
Brayden Moore <braydenmoore2001@gmail.com>
2025-12-04 12:54:03 -0800

    c splash

--- splash/splash/splash.c
+++ splash/splash/splash.c
@@ -31,7 +31,6 @@ void LCD_2IN_splash_display(void)
     }
 	
     Paint_NewImage(BlackImage, LCD_2IN_WIDTH, LCD_2IN_HEIGHT, 90, WHITE, 16);
-    Paint_SetRotate(ROTATE_270);
     Paint_Clear(WHITE);
 
     // --- CORE SPLASH LOGIC ---
