9f8125233ab4ad9aa3eed2a9becc843512ab321e
Brayden Moore <braydenmoore2001@gmail.com>
2025-12-05 17:16:49 -0800

    speed

--- radio.py
+++ radio.py
@@ -1193,8 +1193,8 @@ def periodic_update():
                     updated_count = 0
                     updated_streams = []
                     for name, v in info.items():
-                        if (name in streams.keys()):
-                            if (v['oneLiner'] != streams[name]['oneLiner']):
+                        if (name in streams.keys()) or (len(cached_everything_dict)==0):
+                            if (v['oneLiner'] != streams[name]['oneLiner']) or (len(cached_everything_dict)==0):
                                 updated_streams.append(name)
                                 streams[name].update(v)
                                 updated_count += 1
