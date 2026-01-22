9db76d7620b91e966a00c37706c7f6c9ae9e4b49
Brayden Moore <braydenmoore2001@gmail.com>
2026-01-21 22:28:01 -0800

    refresh cache ignore when stream no longer sshown

--- radio.py
+++ radio.py
@@ -312,7 +312,7 @@ def get_stream_list(stream_dict):
         #fav_start_idx = round(len(stream_list) / 2) - round(len(favorites) / 2)
         #front_half = [i for i in stream_list if i not in favorites][:fav_start_idx]
         #back_half = [i for i in stream_list if i not in favorites and i not in front_half]
-        stream_list =  sorted(favorites, key=str.casefold) + sorted([i for i in stream_list if i not in favorites], key=str.casefold)
+        stream_list =  sorted([i for i in favorites if i in stream_list], key=str.casefold) + sorted([i for i in stream_list if i not in favorites], key=str.casefold)
     
     return stream_list
 
