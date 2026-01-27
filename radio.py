826a769b4c7019a35acac1ce5a5c48a1910fe2f7
Brayden Moore <braydenmoore2001@gmail.com>
2026-01-27 11:42:57 -0800

    ordered cache refresh on fav

--- radio.py
+++ radio.py
@@ -462,7 +462,7 @@ def calculate_ticks():
     draw = ImageDraw.Draw(image) 
     tick_locations = {}
 
-    draw.rectangle([0, tick_start_y-4, SCREEN_WIDTH, tick_start_y+tick_bar_height], fill=BLACK)
+    draw.rectangle([0, tick_start_y-6, SCREEN_WIDTH, tick_start_y+tick_bar_height], fill=BLACK)
     
     total_ticks = len(stream_list)
     mark_width = round(total_span / (total_ticks))
