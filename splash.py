dc52e882b2ccc241a901421498ba102ded5a5483
Brayden Moore <braydenmoore2001@gmail.com>
2026-02-16 15:45:51 -0800

    splash edits

--- splash.py
+++ splash.py
@@ -2,36 +2,34 @@
 import time
 start = time.time()
 
-# Log to file so we can see what's happening
 with open('/tmp/splash-boot.log', 'w') as f:
     f.write(f"Script started at boot time: {time.time()}\n")
-    f.write(f"[{time.time()-start:.2f}s] Starting imports\n")
 
 from PIL import Image
 import driver as LCD_2inch
-
-with open('/tmp/splash-boot.log', 'a') as f:
-    f.write(f"[{time.time()-start:.2f}s] Imports done\n")
 
 SCREEN_WIDTH = 320
 SCREEN_HEIGHT = 240
 
 disp = LCD_2inch.LCD_2inch()
 disp.Init()
-disp.clear()
 disp.bl_DutyCycle(100)
+disp.clear()
 
 image = Image.new('RGB', (SCREEN_WIDTH, SCREEN_HEIGHT))
 bg = Image.open('assets/scud_splash_1.png')
 image.paste(bg, (0, 0))
 
-with open('/tmp/splash-boot.log', 'a') as f:
-    f.write(f"[{time.time()-start:.2f}s] About to display\n")
-
 disp.ShowImage(image)
 
 with open('/tmp/splash-boot.log', 'a') as f:
-    f.write(f"[{time.time()-start:.2f}s] DISPLAYED!\n")
+    f.write(f"[{time.time()-start:.2f}s] First display\n")
+
+for i in range(20):
+    time.sleep(1)
+    disp.ShowImage(image)
+    with open('/tmp/splash-boot.log', 'a') as f:
+        f.write(f"[{time.time()-start:.2f}s] Redraw {i}\n")
 
 while True:
     time.sleep(1)
