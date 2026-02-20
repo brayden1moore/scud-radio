1a4c6bd81ba6319ccca77415db354a162b0d3bb7
Brayden Moore <braydenmoore2001@gmail.com>
2026-02-20 14:51:17 -0800

    click on current stream fix

--- radio.py
+++ radio.py
@@ -933,6 +933,7 @@ def on_button_released():
     button_released_time = current_time
 
     if (button_released_time - button_press_time < 2):
+        print('READIED - ', readied_stream)
         if readied_stream:
             display_one(readied_stream)
             confirm_seek()
