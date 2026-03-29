e33d69e43138cb8dd53df8e8df4f0bc358cd7858
Brayden Moore <braydenmoore2001@gmail.com>
2026-03-29 16:24:37 -0700

    noto

--- radio.py
+++ radio.py
@@ -67,8 +67,8 @@ def load_noto(size, weight=400):
 
 EVERYTHING_INFO_FONT = load_noto(17, weight=400)  
 ONE_INFO_FONT = EVERYTHING_INFO_FONT
-ONE_LARGE_FONT = load_noto(38, 700)
-ONE_LARGISH_FONT = load_noto(32, 700)
+ONE_LARGE_FONT = load_noto(38, 300)
+ONE_LARGISH_FONT = load_noto(32, 300)
 
 #MEDIUM_FONT    = load_noto(18, weight=300)
 #LARGE_FONT     = load_noto(42, weight=300)
