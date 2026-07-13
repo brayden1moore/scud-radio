5fae185668241a0e8c68538e324187e374ba6b20
Brayden Moore <braydenmoore2001@gmail.com>
2026-07-13 13:02:51 -0700

    fav behavior

--- radio.py
+++ radio.py
@@ -576,10 +576,7 @@ def display_everything(name, readied=False, silent=False):
 
         # draw info
         y_offset = 0
-        if title_font == SMALL_LIGHT:
-            everything_info_y = name_chunk_start + 30
-        else:
-            everything_info_y = name_chunk_start + 26
+        everything_info_y = name_chunk_start + 30
         for i in title_lines:
             draw.text((name_chunk_start_x, everything_info_y + y_offset), i, font=title_font, fill=WHITE)
             y_offset += 20
