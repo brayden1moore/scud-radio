287f5b85321f892906c4a8dc50678cb9dd48f094
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-19 10:56:17 -0700

    style

--- radio.py
+++ radio.py
@@ -532,9 +532,9 @@ square_start = padding
 total_span = SCREEN_WIDTH - (2 * padding)
 tick_start = 0  
 tick_bar_height = 25
-tick_bar_start = logo_chunk_start + 90
+tick_bar_start = logo_chunk_start + 90 - 3
 tick_height = 1
-tick_start_y = (tick_bar_start + tick_bar_height / 2) - 3
+tick_start_y = (tick_bar_start + tick_bar_height / 2) 
 tick_image = None
 tick_locations = {}
 
