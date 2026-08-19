6026d872f4e6f26dc9a799b23139991ae48d6be3
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-19 10:19:05 -0700

    styling

--- radio.py
+++ radio.py
@@ -110,9 +110,11 @@ def load_font(name, size, weight=400):
         font.set_variation_by_axes([weight]) 
     return font
 
-print('LOADING NOTO',time.time())
-SMALL_LIGHT = load_font('Noto', 17, weight=400)  
-print('LOADING ARCHIVO',time.time())
+print('LOADING SMALL',time.time())
+#SMALL_LIGHT = load_font('Noto', 17, weight=400)  
+SMALL_LIGHT = load_font('Favorit', 17, weight=400)  
+
+print('LOADING LARGE',time.time())
 EXTRALARGE_LIGHT = load_font('Archivo',38, weight=800)  
 
 def replace_font(font):
@@ -787,7 +789,7 @@ def display_scroll(name, silent=False):
                 top = bbox[1]
                 fill = RED if idx == 0 else BLUE if idx == 1 else YELLOW
                 x0 = tags_start_x + genre_x_offset
-                draw.rectangle([x0, tags_start_y, x0 + genre_width, tags_start_y + 1 + box_h], fill=fill, outline=BLACK, width=3)
+                draw.rectangle([x0 - 1, tags_start_y -1 , x0 + genre_width + 1, tags_start_y + 1 + box_h + 1], fill=fill, outline=BLACK, width=3)
                 draw.text((x0, tags_start_y - top + 1), genre, font=info_font, fill=BLACK)
                 genre_x_offset += genre_width + 5
 
