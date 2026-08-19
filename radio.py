13d80c7409edca4a93465644cfec550c7b790133
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-19 10:46:22 -0700

    styling

--- radio.py
+++ radio.py
@@ -846,6 +846,7 @@ def display_scroll(name, silent=False):
             image.paste(star_96, og_logo_position, star_96)
         
         draw.rectangle([og_logo_position[0], og_logo_position[1], og_logo_position[0]+96, og_logo_position[1]+96], outline=BLACK, width=3) # border
+        draw.rectangle([og_logo_position[0] - 2, og_logo_position[1] - 2, og_logo_position[0]+96 + 2, og_logo_position[1]+96 + 2], outline=WHITE, width=3) # border
 
 
         # draw marks
