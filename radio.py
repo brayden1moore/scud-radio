c49d96a3d22583a67e39257590715c5e5dd25fbe
Brayden Moore <braydenmoore2001@gmail.com>
2026-08-19 10:39:37 -0700

    styling

--- radio.py
+++ radio.py
@@ -818,17 +818,20 @@ def display_scroll(name, silent=False):
         # double prev and next
         double_prev_logo_size = 96
         double_prev_position = (square_start, logo_chunk_start + 57 - 4)
-        double_next_position = (290, logo_chunk_start + 57 - 4)     
+        double_next_position = (290, logo_chunk_start + 57 - 4)  
+
+        double_prev_position = (prev_position[0] - 70, og_logo_position[1])
+        double_prev_position = (next_position[0] + 70, og_logo_position[1])
         double_prev = streams[double_prev_stream][f'logo_{double_prev_logo_size}']
         double_next = streams[double_next_stream][f'logo_{double_prev_logo_size}']
         
-        #image.paste(double_prev, double_prev_position)
+        image.paste(double_prev, double_prev_position)
         draw.rectangle([double_prev_position[0],double_prev_position[1], double_prev_position[0] + double_prev_logo_size, double_prev_position[1] + double_prev_logo_size], outline=BLACK, width=3)
         if double_prev_stream in favorites:
             double_prev_star = star_25.copy()
             image.paste(double_prev_star, double_prev_position, double_prev_star)
 
-        #image.paste(double_next, double_next_position)
+        image.paste(double_next, double_next_position)
         draw.rectangle([double_next_position[0],double_next_position[1], double_next_position[0] + double_prev_logo_size, double_next_position[1] + double_prev_logo_size], outline=BLACK, width=3)
         if double_next_stream in favorites:
             double_next_star = star_25.copy()
